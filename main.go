2aad55760307cc5b0f85694f5f204d9a10ced175
rmcdougald <45009416+rmcdougald@users.noreply.github.com>
2023-08-30 11:49:43 -0400

    change awesome to great

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	myFigure := figure.NewColorFigure("rmcdougald is Awesome!!!", "larry3d", "yellow", true)
+	myFigure := figure.NewColorFigure("rmcdougald is Great!!!", "larry3d", "yellow", true)
 	myFigure.Print()
 
 	if secret := os.Getenv("SECRET"); secret != "" {
